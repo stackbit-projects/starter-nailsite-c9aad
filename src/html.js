4eb398d2909d339805723f359aebb84702349e00
Stackbit Code Editor <projects@stackbit.com>
2021-08-30 11:09:17 +0000

    html.js: changed

--- src/html.js
+++ src/html.js
@@ -9,6 +9,9 @@ export default function HTML(props) {
         <link rel="preconnect" href="https://app.snipcart.com">
         <link rel="preconnect" href="https://cdn.snipcart.com">
         <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.css" />
+        <button class="snipcart-checkout">
+  Cart
+</button>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
